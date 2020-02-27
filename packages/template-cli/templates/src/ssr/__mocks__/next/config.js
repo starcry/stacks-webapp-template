430503032ddfb8bc0ef8ea6d327987fc8531f511
Naren Bellala <naren.bellala@amido.com>
2020-02-27 16:03:17 +0000

    chore: initial commit

--- packages/template-cli/templates/src/ssr/__mocks__/next/config.js
+++ packages/template-cli/templates/src/ssr/__mocks__/next/config.js
@@ -1,3 +1,6 @@
 module.exports = () => ({
     publicRuntimeConfig: {},
+    prepare: () => Promise.resolve(),
+    getRequestHandler: () => (req, res) => res.send("Welcome to Next.js!"),
+    renderToHTML: () => Promise.resolve("Welcome to Next.js!"),
 })
